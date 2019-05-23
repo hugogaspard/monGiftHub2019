a8e3d71cc13ba08b3fff92e57f2de64ea3b126e3
hugogaspard <50944400+hugogaspard@users.noreply.github.com>
2019-05-23 10:53:45 +0200

    Update Gifthubclass.java

--- src/gifthugPackage/Gifthubclass.java
+++ src/gifthugPackage/Gifthubclass.java
@@ -2,4 +2,15 @@ package gifthugPackage;
 
 public class Gifthubclass {
 
+		Item i1 = new Item (1000001, "Lucky Luke T1", 12.50); 
+		Item i2 = new Item (1000002, "Tif et Tondu T2", 8.70); 
+		Item i3 = new Item (1000003, "Largo Winch T12", 14.20); 
+		Item i4 = new Item (1000004, "Persepolis Integrale", 21.80); 
+	
+		Cart c = new Cart(); 
+		c.add(i1,2);
+		c.add(i2,8);
+		c.add(i3,3);
+		c.add(i4,12);
+	
 }
