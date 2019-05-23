93ad4709ac33fd343fa573371392924a0ef74055
hugogaspard <50944400+hugogaspard@users.noreply.github.com>
2019-05-23 13:22:13 +0200

    Update Gifthubclass.java

--- src/gifthugPackage/Gifthubclass.java
+++ src/gifthugPackage/Gifthubclass.java
@@ -1,6 +1,18 @@
 package gifthugPackage;
 
 public class Gifthubclass {
+	public static void main(String[] args) {
+		Parent parent = new Parent ("prenomParent", "nomParent", 10);
+		parent.FaireEnfants();
+		
+		parent.grandir();
+		parent.FaireEnfants();
+		
+		List<Parent>list1 = new ArrayList<>() ;
 
-Et voilà 
+		for (Person s:list1) {
+			System.out.println(s);
+			
+		}
+	}
 }
